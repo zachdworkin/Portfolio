7a7c5220a4e89cf41e2a0a3c96206f5a1f6b72fd
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-04 14:44:18 -0800

    Create JcfHashSetTest.java

---
+++ CS II/High Scores 2/JcfHashSetTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JcfHashSetTest {
+
+	private Set<Integer> set;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		set = new JcfHashSet<Integer>();
+	}
+
+	@Test
+	public void addsItems() {
+		assertFalse(set.contains(8));
+		set.add(8);
+		assertTrue(set.contains(8));
+		set.add(3);
+		assertTrue(set.contains(3));
+		assertTrue(set.contains(8));
+	}
+
+	@Test
+	public void removesItems() {
+		set.add(8);
+		set.remove(8);
+		assertFalse(set.contains(8));
+	}
+
+	@Test
+	public void addsManyItems() {
+		int[] numbers = new int[1000];
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = StdRandom.uniform(Integer.MAX_VALUE);
+		}
+		for (int n : numbers) {
+			set.add(n);
+		}
+		for (int n : numbers) {
+			assertTrue(set.contains(n));
+		}
+	}
+
+	@Test
+	public void iterates() {
+		int[] numbers = {8, 6, 7, 5, 3, 0, 9};
+		for (int n : numbers) {
+			set.add(n);
+		}
+		java.util.Arrays.sort(numbers);
+		int i = 0;
+		for (int n : set) {
+			assertTrue(java.util.Arrays.binarySearch(numbers, n) >= 0);
+			i++;
+		}
+		assertEquals(numbers.length, i);
+	}
+
+}
