35494bc80bae021df9fed06c008da70da905a22c
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-04 14:56:56 -0800

    Create QuestionsModelTest.java

---
+++ CS II/Questions/QuestionsModelTest.java
@@ -0,0 +1,82 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuestionsModelTest {
+
+	private QuestionsModel model;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		model = new QuestionsModel();
+	}
+
+	@Test
+	public void hasDefaultInitialTree() {
+		assertEquals(
+				"Does it have a motor?\n  Does it store information?\n    a hard drive\n    a car\n  a giraffe\n",
+				model.toString());
+	}
+
+	@Test
+	public void getsRoot() {
+		assertEquals("Does it have a motor?", model.getRoot().getKey());
+	}
+
+	@Test
+	public void getsCurrentNode() {
+		assertSame(model.getRoot(), model.getCurrentNode());
+	}
+
+	@Test
+	public void descends() {
+		model.descend(true);
+		assertEquals("Does it store information?", model.getCurrentNode()
+				.getKey());
+		model.descend(false);
+		assertEquals("a car", model.getCurrentNode().getKey());
+	}
+
+	@Test
+	public void detectsLeaf() {
+		model.descend(true);
+		assertFalse(model.atLeaf());
+		model.descend(false);
+		assertTrue(model.atLeaf());
+	}
+
+	@Test
+	public void resets() {
+		model.descend(true);
+		model.descend(false);
+		model.reset();
+		assertSame(model.getRoot(), model.getCurrentNode());
+	}
+
+	@Test
+	public void getsQuestion() {
+		model.descend(true);
+		assertEquals("Does it store information? (y/n)", model.getQuestion());
+		model.descend(false);
+		assertEquals("Is it a car? (y/n)", model.getQuestion());
+	}
+
+	@Test
+	public void getsLearningQuestion() {
+		model.descend(false);
+		assertEquals(
+				"What question would distinguish a fox (y) from a giraffe (n)?",
+				model.getLearningQuestion("a fox"));
+	}
+
+	@Test
+	public void learns() {
+		model.descend(false);
+		model.learn("a lion", "Is it carnivorous?");
+		assertEquals(
+				"Does it have a motor?\n  Does it store information?\n    a hard drive\n    a car\n  Is it carnivorous?\n    a lion\n    a giraffe\n",
+				model.toString());
+	}
+
+}
