a24a0245628344235701dedb836b94fd8c6f5e8d
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-04 14:32:39 -0800

    Create DieTest.java

---
+++ CS II/Beetle/DieTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DieTest {
+
+	@Test
+	public void initialTopFaceIs1() {
+		Die d = new Die();
+		assertEquals(1, d.getTopFace());
+	}
+
+	@Test
+	public void allNumbersAreEquallyLikely() {
+		Die d = new Die();
+		int[] counts = new int[7];
+		// Roll the die many times
+		for (int i = 0; i < 600; i++) {
+			d.roll();
+			counts[d.getTopFace()]++;
+		}
+		// Each number should be about equally likely
+		for (int i = 1; i <= 6; i++) {
+			assertTrue(counts[i] > 50);
+			assertTrue(counts[i] < 150);
+		}
+	}
+
+	@Test
+	public void noExtraneousNumbersAppear() {
+		Die d = new Die();
+		// Roll the die many times
+		for (int i = 0; i < 600; i++) {
+			d.roll();
+			assertTrue(d.getTopFace() >= 1);
+			assertTrue(d.getTopFace() <= 6);
+		}
+	}
+
+	@Test
+	public void multipleDiceCanShowDifferentTopFaces() {
+		Die d1 = new Die();
+		Die d2 = new Die();
+		int counter = 0;
+		for (int i = 0; i < 100; i++) {
+			d1.roll();
+			d2.roll();
+			if (d1.getTopFace() == d2.getTopFace()) {
+				counter++;
+			}
+		}
+		assertTrue(counter > 0);
+		assertTrue(counter < 50);
+	}
+
+}
