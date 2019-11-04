baeb337758db19f18846c6ee32e4f8a8692caed7
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-04 08:51:51 -0800

    Add files via upload

---
+++ BackGammon/src/Die.java
@@ -0,0 +1,20 @@
+/** Die Class saves value of a die */
+public class Die {
+
+    private int value;
+
+    /** countstuctor*/
+    public Die(){
+    }
+
+    /** return the die value*/
+    public int getValue() {
+        return value;
+    }
+
+    /**change the die value*/
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+}
