5f63894dcbafe2690817865098e3c902df4c85b9
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-04 15:07:22 -0800

    Create Hanoi.java

---
+++ CS II/Towers of Hanoi/Hanoi.java
@@ -0,0 +1,58 @@
+public class Hanoi {
+
+	public static void main(String[] args) { hanoi("A","B","C",3);
+	}
+	
+	public static void hanoi1HardCoded(String start, String end) {
+		StdOut.println(start + " -> " + end);		
+	}
+	
+	public static void hanoi2HardCoded(String start, String spare, String end) {
+		StdOut.println(start + " -> " + spare);		
+		StdOut.println(start + " -> " + end);		
+		StdOut.println(spare + " -> " + end);		
+	}
+	
+	public static void hanoi3HardCoded(String start, String spare, String end) {
+		StdOut.println(start + " -> " + end);		
+		StdOut.println(start + " -> " + spare);		
+		StdOut.println(end + " -> " + spare);		
+		StdOut.println(start + " -> " + end);		
+		StdOut.println(spare + " -> " + start);		
+		StdOut.println(spare + " -> " + end);		
+		StdOut.println(start + " -> " + end);		
+	}
+	
+	public static void hanoi1CallingSimplerMethods(String start, String end) {
+		StdOut.println(start + " -> " + end);
+	}
+	
+	public static void hanoi2CallingSimplerMethods(String start, String spare, String end) {
+		hanoi1CallingSimplerMethods(start, spare);		
+		StdOut.println(start + " -> " + end);		
+		hanoi1CallingSimplerMethods(spare, end);		
+	}
+	
+	public static void hanoi3CallingSimplerMethods(String start, String spare, String end) {
+		hanoi2CallingSimplerMethods(start, end, spare);		
+		StdOut.println(start + " -> " + end);		
+		hanoi2CallingSimplerMethods(spare, start, end);
+	}
+
+	public static void hanoi4CallingSimplerMethods(String start, String spare, String end) {
+		hanoi3CallingSimplerMethods(start, spare, end);
+		StdOut.println(start + " -> " + end);
+		hanoi3CallingSimplerMethods(spare, start, end);
+	}
+
+	public static void hanoi(String start, String spare, String end, int n) {
+		if (n == 1) {
+			StdOut.println(start + " -> " + end);
+		} else {
+			hanoi(start, end, spare, n-1);
+			StdOut.println(start + " -> " + end);
+			hanoi(spare, start, end, n-1);
+		}
+	}
+
+}
