fc23d3b4ea29538aa934f7507b9aa0e97d82d3ef
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-04 15:23:10 -0800

    Create bitlib.h

---
+++ CS ARCHITECTURE/Bit Manipulation/bitlib.h
@@ -0,0 +1,8 @@
+/// bit manip functions
+
+int bitwise_nor(int x, int y);
+int bitwise_xor(int x, int y);
+int eval_not_equal(int x, int y);
+int get_byte(int x, int n);
+int copy_lsbit(int x);
+int bit_count(int x);
