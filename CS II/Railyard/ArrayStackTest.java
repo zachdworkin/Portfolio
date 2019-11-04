66325b4760ae157181224c8265d30a8a91119a07
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-04 14:58:34 -0800

    Create ArrayStackTest.java

---
+++ CS II/Railyard/ArrayStackTest.java
@@ -0,0 +1,63 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayStackTest {
+
+	private ArrayStack stack;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		stack = new ArrayStack(5);
+	}
+
+	@Test
+	public void grows() {
+		assertEquals(0, stack.size());
+		stack.add(23);
+		assertEquals(1, stack.size());
+		stack.add(42);
+		assertEquals(2, stack.size());
+	}
+
+	@Test
+	public void detectsEmptiness() {
+		assertTrue(stack.isEmpty());
+		stack.add(17);
+		assertFalse(stack.isEmpty());
+		stack.add(88);
+		stack.remove();
+		assertFalse(stack.isEmpty());
+		stack.remove();
+		assertTrue(stack.isEmpty());
+	}
+	
+	@Test
+	public void removesLifo() {
+		stack.add(1);
+		stack.add(2);
+		assertEquals(2, stack.remove());
+		assertEquals(1, stack.remove());
+	}
+
+	@Test
+	public void getsItemAtSpecifiedIndex() {
+		stack.add(5);
+		stack.add(38);
+		assertEquals(5, stack.get(0));
+		assertEquals(38, stack.get(1));
+	}
+
+	@Test
+	public void detectsFullness() {
+		stack.add(1);
+		stack.add(1);
+		stack.add(1);
+		stack.add(1);
+		assertFalse(stack.isFull());
+		stack.add(1);
+		assertTrue(stack.isFull());
+	}
+
+}
