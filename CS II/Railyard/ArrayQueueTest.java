10689f7ded6ec24236cb64b7bf149459fa30da46
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-04 14:58:25 -0800

    Create ArrayQueueTest.java

---
+++ CS II/Railyard/ArrayQueueTest.java
@@ -0,0 +1,63 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayQueueTest {
+
+	private ArrayQueue queue;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		queue = new ArrayQueue(5);
+	}
+
+	@Test
+	public void grows() {
+		assertEquals(0, queue.size());
+		queue.add(23);
+		assertEquals(1, queue.size());
+		queue.add(42);
+		assertEquals(2, queue.size());
+	}
+
+	@Test
+	public void detectsEmptiness() {
+		assertTrue(queue.isEmpty());
+		queue.add(17);
+		assertFalse(queue.isEmpty());
+		queue.add(88);
+		queue.remove();
+		assertFalse(queue.isEmpty());
+		queue.remove();
+		assertTrue(queue.isEmpty());
+	}
+	
+	@Test
+	public void removesFifo() {
+		queue.add(1);
+		queue.add(2);
+		assertEquals(1, queue.remove());
+		assertEquals(2, queue.remove());
+	}
+
+	@Test
+	public void getsItemAtSpecifiedIndex() {
+		queue.add(5);
+		queue.add(38);
+		assertEquals(5, queue.get(0));
+		assertEquals(38, queue.get(1));
+	}
+
+	@Test
+	public void detectsFullness() {
+		queue.add(1);
+		queue.add(1);
+		queue.add(1);
+		queue.add(1);
+		assertFalse(queue.isFull());
+		queue.add(1);
+		assertTrue(queue.isFull());
+	}
+
+}
