0f88eb418f08f17fb1805351e28aed80b405386e
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-04 14:30:20 -0800

    Create ExtentTest.java

---
+++ CS II/Asteroid Rally/ExtentTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExtentTest {
+
+	public static final double DELTA = 0.001;
+	
+	private Extent extent;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		extent = new Extent(0.5, 0.3, 0.2);
+	}
+
+	@Test
+	public void storesX() {
+		assertEquals(0.5, extent.getX(), DELTA);
+	}
+	
+	@Test
+	public void storesY() {
+		assertEquals(0.3, extent.getY(), DELTA);
+	}
+	
+	@Test
+	public void storesRadius() {
+		assertEquals(0.2, extent.getRadius(), DELTA);
+	}
+	
+	@Test
+	public void moves() {
+		extent.move(0.1, -0.2);
+		assertEquals(0.6, extent.getX(), DELTA);
+		assertEquals(0.1, extent.getY(), DELTA);
+		assertEquals(0.2, extent.getRadius(), DELTA);		
+	}
+
+	@Test
+	public void findsDistance() {
+		Extent a = new Extent(0.3, 0.0, 0.1);
+		Extent b = new Extent(0.0, 0.4, 0.8);
+		assertEquals(0.5, a.distanceTo(b), DELTA);
+	}
+
+	@Test
+	public void detectsOverlap() {
+		assertTrue(new Extent(1, 0, 1.0).overlaps(new Extent(4, 0, 2.1)));
+	}
+
+	@Test
+	public void detectsLackOfOverlap() {
+		assertFalse(new Extent(0, 1, 1.0).overlaps(new Extent(0, 4, 1.9)));
+	}
+
+	@Test
+	public void detectsDiagonalOverlap() {
+		assertTrue(new Extent(0, 0, 1.0).overlaps(new Extent(-1, -1, 1.0)));
+	}
+
+}
