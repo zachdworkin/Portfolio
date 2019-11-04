8de8a69edd5f3ef5e9f74dc5822e2e3e48cfa7f6
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-04 15:04:55 -0800

    Create Treasure.java

---
+++ CS II/Text Adventure/Treasure.java
@@ -0,0 +1,43 @@
+/** A treasure. */
+public class Treasure {
+
+	/** A description of this treasure. */
+	private String description;
+
+	/** This treasure's name. */
+	private String name;
+
+	/** The value of this treasure. */
+	private int value;
+
+	/**
+	 * @param name
+	 *            This treasure's name.
+	 * @param value
+	 *            The value of this treasure.
+	 * @param description
+	 *            A description of this treasure.
+	 */
+	public Treasure(String name, int value, String description) {
+		//sets the private variables for the treasure
+		this.name = name;
+		this.value = value;
+		this.description = description;
+	}
+
+	/** Returns a description of this treasure. */
+	public String getDescription() {
+		return description;
+	}
+
+	/** Returns this treasure's name. */
+	public String getName() {
+		return name;
+	}
+
+	/** Returns the value of this treasure. */
+	public int getValue() {
+		return value;
+	}
+
+}
