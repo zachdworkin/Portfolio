5464660160388f0904c89e704f2b260ea4cce602
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-04 14:20:50 -0800

    Create Spotted.c

---
+++ CS I/Spotted.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+  //finds the largest number in a list of numbers
+  double x,count,first;
+  x=0;//initialize
+  count=0;//initialize
+  printf("Enter the first positive number and enter -1 after the last number:\n");
+  scanf("%lf",&x);
+  first=x;
+  while(x!=-1){
+      if(x==first){
+      count=count+1;
+    }
+    printf("Enter the next number:\n");
+    scanf("%lf",&x);
+  }
+  if(count>1){
+    printf("Yes\n");
+  }else{
+    printf("No\n");
+  }
+}
