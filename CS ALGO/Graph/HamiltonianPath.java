1cfdff1f4773533c9e599c77ee381d3bf6b2a67f
Zach Dworkin <39574476+zachdworkin@users.noreply.github.com>
2019-11-18 12:41:15 -0800

    Update HamiltonianPath.java

--- CS ALGO/Graph/HamiltonianPath.java
+++ CS ALGO/Graph/HamiltonianPath.java
@@ -3,18 +3,33 @@ import edu.princeton.cs.algs4.EdgeWeightedDigraph;
 import edu.princeton.cs.algs4.Topological;
 
 public class HamiltonianPath {
-    public static void main(String[] args) {
-        new HamiltonianPath().run();
-    }
+
+    /**
+     * This code solves the Hamiltonian Path problem on rosalind.info
+     * By Zach Dworkin
+     */
+
+    public static void main(String[] args) { new HamiltonianPath().run(); }
+
+    /**
+     * Runs the solve method for each graph provided in the data set
+     */
     private void run(){
 //        In in = new In("HamiltonianPath.txt");
         In in = new In("rosalind_hdag.txt");
         int n = in.readInt();//number of graphs
         for (int i = 0;i < n; i++) {
-            solve(in);
+            StdOut.println(solve(in));
         }
     }
 
+    /**
+     * Determines if there is an edge between i->j
+     * @param g graph being utilized
+     * @param i start node
+     * @param j end node
+     * @return
+     */
     public boolean hasEdge(EdgeWeightedDigraph g, int i, int j){
         if(i==-1){
             return true;
@@ -27,34 +42,36 @@ public class HamiltonianPath {
         return false;
     }
 
-    private void solve(In in) {
-        boolean flag=false;
+    /**
+     *
+     * @param in the edgeweighted digraph being used to solve the problem
+     * @return the answer in the form of a string
+     */
+    private String solve(In in) {
         EdgeWeightedDigraph g = readGraph(in);
         Topological top = new Topological(g);
         int current = -1;
         int next = -1;
+        String finished = "1 ";
         for(int a : top.order()){
             current=next;
             next=a;
             if(a!=0) {
                 if (!hasEdge(g, current, next)) {
-                    flag = !flag;
-                    break;
+                    return "-1";
+                }else{
+                    finished=finished+a+" ";
                 }
             }
         }
-        if(!flag){
-            StdOut.print("1 ");
-            for(int a : top.order()) {
-                if(a!=0) {
-                    StdOut.print(a + " ");
-                }
-            }
-            StdOut.println();
-        }else{
-            StdOut.println("-1");
-        }
+        return finished;
     }
+
+    /**
+     * reads in a graph from the data set
+     * @param in
+     * @return
+     */
     private EdgeWeightedDigraph readGraph(In in) {
         int v= in.readInt();
         int e=in.readInt();
